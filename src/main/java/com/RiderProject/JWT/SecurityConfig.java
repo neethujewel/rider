9ea1c48988dfa6bf5e8cb8ba8d3b5a3fae39e892
Neethu Jewel <neethu.ptf@gmail.com>
2024-05-22 12:15:50 +0530

    Rider

--- src/main/java/com/RiderProject/JWT/SecurityConfig.java
+++ src/main/java/com/RiderProject/JWT/SecurityConfig.java
@@ -29,8 +29,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/authenticate").permitAll()
                 .anyRequest().authenticated()
